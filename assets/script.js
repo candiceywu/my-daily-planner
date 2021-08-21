0885e51af69c6c812c15875bca6366dcd2cbcc6a
candiceywu <candice.ying.wu@gmail.com>
2021-08-20 17:58:47 -0700

    creating local storage

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,111 @@
 // Use Moment.js to format the calendar date by: day of the week, Month + dayth
-
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-
 //prevent 'enter' key from being used; user must click save button
-$(document).on("keydown", "form", function(event) { 
-    return event.key != "Enter";
+$(document).on("keydown", "form", function (event) {
+  return event.key != "Enter";
 });
 
 
-var activityFormEl = $('#activity-form');
-var activityListEl = $('#activity-list');
 
-function submitForm(event) {
-  event.preventDefault();
+//logging entry and using button to save
 
-  activityItem = $('input[name="activity-input"]').val();
+var activityEl = $(".col-sm-8 col-md-8 col-lg-8");
+var activityList = $("#activity-list");
+var button = $(".btn btn-info");
+var btn1 = $("#btn-1");
+var btn2 = $("#btn-2");
+var activityArray1 = [];
+var activityArray2 = [];
 
-  if (!activityItem) {
-    console.log('No activities have been scheduled for this time.');
-    return;
-  }
-
-  //creates list for logged activities 
-  var activityListItemEl = $('<li class="activity">');
-  activityListItemEl.text(activityItem);
+//8am timeslot
+btn1.on("click", function () {
+  console.log("button 1 clicked");
+  var inputText = $("#activity-input-1").val();
+  activityArray1.push(inputText);
+  console.log(activityArray1);
 
 
-  // print to the page
-  activityListEl.append(activityListItemEl);
 
-//   // clear the form input element
-//   $('input[name="activity-input"]').val('');
+  localStorage.setItem("8AM", JSON.stringify(activityArray1));
+
+});
+
+//9am timeslot
+btn2.on("click", function () {
+  console.log("button 2 clicked");
+  var inputText = $("#activity-input-2").val();
+  activityArray2.push(inputText);
+  console.log(activityArray2);
+
+ 
+  // var activityListItemEl = $('<li class="list">');
+  // activityListItemEl.text(inputText);
+
+  // // print to the page
+  // activityList.append(activityListItemEl);
+
+
+  localStorage.setItem("9AM", JSON.stringify(activityArray2));
+});
+
+
+
+
+
+
+//local storage - want to store an array to local storage
+// use json stringify to convert array to string; then use setItem to put into local storage
+
+//use getItem from local storage; use JSON to parse to turn back into array to display
+
+
+
+
+// button.on("click", function () {
+
+//   var activityItem = $('input[name="activity-input"]').val();
+
+//   if (!activityItem) {
+//     console.log('No activities have been planned for this hour');
+//     return;
+//   }
+
+//   var activityListItemEl = $('<li class="list">');
+//   activityListItemEl.text(activityItem);
+
+//   // print to the page
+//   activityListEl.append(activityListItemEl);
+//   console.log("button was pressed");
+// })
+
+
+
+// function submit(event) {
+
+
+
+//   var activityItem = $('input[name="activity-input"]').val();
+
+//   if (!activityItem) {
+//     console.log('No activities have been planned for this hour');
+//     return;
+//   }
+
+//   var activityListItemEl = $(
+//     '<li class="list">'
+//   );
+//   activityListItemEl.text(activityItem);
+
+//   // print to the page
+//  activityListEl.append(activityListItemEl);
 // }
 
-//need to create .on for when button is clicked, activity item is logged into <li>
+// activityFormEl.click('submit', submit);
 
-activityFormEl.on('click', submitForm);
+
+
+
+
+
--- copyjs.js
+++ copyjs.js
@@ -1,13 +1,13 @@
-var activityFormEl = $('#activity-form');
-var activityListEl = $('#activity-list');
+var shoppingFormEl = $('#activity-form');
+var shoppingListEl = $('#activity-list');
 
-function submitForm(event) {
+function handleFormSubmit(event) {
   event.preventDefault();
 
-  activityItem = $('input[name="activity-input"]').val();
+  var activityItem = $('input[name="activity-input"]').val();
 
   if (!activityItem) {
-    console.log('No activities have been scheduled for this time.');
+    console.log('No activities have been planned for this hour.');
     return;
   }
 
@@ -16,26 +16,26 @@ function submitForm(event) {
   );
   activityListItemEl.text(activityItem);
 
-//   // add delete button to remove activity list item
-//   activityListItemEl.append(
-//     '<button class="btn btn-danger btn-small delete-item-btn">Remove</button>'
+  // add delete button to remove shopping list item
+ activityListItemEl.append(
+    '<button class="btn btn-danger btn-small delete-item-btn">Remove</button>'
   );
 
   // print to the page
-  activityListEl.append(activityListItemEl);
+ activityListEl.append(activityListItemEl);
 
   // clear the form input element
   $('input[name="activity-input"]').val('');
 }
 
-// function handleRemoveItem(event) {
-//   // convert button we pressed (`event.target`) to a jQuery DOM object
-//   var btnClicked = $(event.target);
+function handleRemoveItem(event) {
+  // convert button we pressed (`event.target`) to a jQuery DOM object
+  var btnClicked = $(event.target);
 
-//   // get the parent `<li>` element from the button we pressed and remove it
-//   btnClicked.parent('li').remove();
+  // get the parent `<li>` element from the button we pressed and remove it
+  btnClicked.parent('li').remove();
 }
 
-// use event delegation on the `activityListEl` to listen for click on any element with a class of `delete-item-btn`
-// activityListEl.on('click', '.delete-item-btn', handleRemoveItem);
-activityFormEl.on('submit', submitForm);
+// use event delegation on the `shoppingListEl` to listen for click on any element with a class of `delete-item-btn`
+activityListEl.on('click', '.delete-item-btn', handleRemoveItem);
+activityFormEl.on('submit', handleFormSubmit);
